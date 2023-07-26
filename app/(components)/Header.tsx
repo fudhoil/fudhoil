c7ef20417de916b967678a0da63a3e28ef7efa86
fudhoil <fudhoilhlf@gmail.com>
2023-07-26 08:50:49 +0700

    new hook

--- app/(components)/Header.tsx
+++ app/(components)/Header.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import LoadingBar from "react-top-loading-bar";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
